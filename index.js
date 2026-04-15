45de971550f5eee7ce1c197831792ca804a0ca43
Rachita Joshi <rachita.joshi@changejar.in>
2026-04-15 17:03:27 +0530

    updated code

--- index.js
+++ index.js
@@ -6,13 +6,24 @@ const app = new App({
 });
 
 app.message(async ({ message, say }) => {
-if (message.text.toLowerCase().includes("leave")) {
-await say("You have 12 Earned Leaves, 8 Sick Leaves and 7 Casual Leaves in a year");
-} else {
-await say("Hi, I’m Jarvis! How can I help?");
-}
-});
+  const text = message.text.toLowerCase();
 
+  if (text.includes("leave")) {
+    await say("You have 12 Earned Leaves, 8 Sick Leaves and 7 Casual Leaves in a year");
+
+  } else if (text.includes("holiday")) {
+    await say("To check your holidays for this calendar year, go to Keka → Home → Dashboard → Holidays");
+
+  } else if (text.includes("salary")) {
+    await say("Salary is credited on the last day of the month. In case this day falls on a weekend, then the salary is credited on last weekday before the last day of the month");
+
+} else if (["how are you", "sup", "whatsup", "wasgud"].some(word => text.includes(word))) {
+  await say("I'm great, how is your day going?");
+
+  } else {
+    await say("Hi, I’m Jarvis! How can I help?");
+  }
+});
 
 (async () => {
   await app.start(3000);
